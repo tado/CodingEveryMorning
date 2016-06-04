82818e9a86f1ff101a2313c446936b4f2dda2208
Atsushi Tadokoro <tadokoro@gmail.com>
2016-06-04 11:21:35 +0900

    update

--- Sketch160605/src/ofApp.cpp
+++ Sketch160605/src/ofApp.cpp
@@ -4,24 +4,13 @@
 void ofApp::setup(){
     ofSetFrameRate(60);
     ofBackground(0);
-    
     light.enable();
-    light.setPosition(-100, 100, 800);
+    light.setPosition(-200, 200, 500);
     ofEnableDepthTest();
-    //myMesh = ofSpherePrimitive(200, 60).getMesh();
-    myMesh.setMode(OF_PRIMITIVE_POINTS);
-    glPointSize(2.0);
-    for (int i = 0; i < 5000; i++) {
-        float x = ofRandom(-ofGetWidth()/6, ofGetWidth()/6);
-        float y = ofRandom(-ofGetWidth()/6, ofGetWidth()/6);
-        float z = ofRandom(-ofGetWidth()/6, ofGetWidth()/6);
-        ofVec3f location = ofVec3f(x, y, z);
-        myMesh.addVertex(location);
+    myMesh = ofSpherePrimitive(200, 60).getMesh();
+    for (int i = 0; i < myMesh.getVertices().size(); i++) {
         myMesh.addColor(ofFloatColor(1.0, 1.0, 1.0, 1.0));
     }
-    
-    box.set(ofGetWidth()/3.0, ofGetWidth()/3.0, ofGetWidth()/3.0);
-    box.setResolution(1);
 }
 
 //--------------------------------------------------------------
@@ -30,32 +19,21 @@ void ofApp::update(){
     float noiseTime = time * 1.0;
     
     for (int i = 0; i < myMesh.getVertices().size(); i++) {
-        ofVec3f loc = myMesh.getVertices()[i] / 1400.0;
-        float r = ofNoise(loc.x, loc.y, loc.z, ofGetElapsedTimef() * 0.5);
-        float g = ofNoise(loc.x, loc.y, loc.z, ofGetElapsedTimef() * 0.6);
-        float b = ofNoise(loc.x, loc.y, loc.z, ofGetElapsedTimef() * 0.7);
-        myMesh.setColor(i, ofFloatColor(r, g, b, 1.0));
+        ofVec3f loc = myMesh.getVertices()[i] / 200.0;
+        float noise = ofMap(ofNoise(loc.x, loc.y, loc.z, ofGetElapsedTimef()), 0, 1, 100, 200);
+        ofVec3f newLoc = loc.normalize()* noise;
+        myMesh.setVertex(i, newLoc);
+        float c = ofMap(ofNoise(loc.x, loc.y, loc.z, ofGetElapsedTimef()),0, 1, 0.5, 1.2);
+        myMesh.setColor(i, ofFloatColor(c*0.5, c*0.7, c, 1.0));
     }
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-    ofHideCursor();
     cam.begin();
     ofPushMatrix();
-    ofRotateX(30);
-    ofRotateY(ofGetElapsedTimef() * 2);
-    //myMesh.draw();
-    for (int i = 0; i < myMesh.getVertices().size(); i++) {
-        ofPushMatrix();
-        ofTranslate(myMesh.getVertices()[i]);
-        ofRotateX(myMesh.getColors()[i].r * 360);
-        ofRotateY(myMesh.getColors()[i].g * 360);
-        ofRotateZ(myMesh.getColors()[i].b * 360);
-        //ofSetColor(myMesh.getColors()[i].r*255, myMesh.getColors()[i].g*255, myMesh.getColors()[i].b*255);
-        ofDrawLine(-10, 0, 10, 0);
-        ofPopMatrix();
-    }
+    ofRotateY(ofGetElapsedTimef()*2.0);
+    myMesh.draw();
     ofPopMatrix();
     cam.end();
 }
